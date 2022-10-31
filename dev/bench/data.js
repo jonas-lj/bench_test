647d15d8d513fbf8623c8208925b5b94e0dec21d
github-action-benchmark <github@users.noreply.github.com>
2022-10-31 10:02:31 +0000

    add My Project Cargo Benchmark (cargo) benchmark result for 7ea8979a4418f248fd1fc12afab789d1c5f3fc0c

---
+++ dev/bench/data.js
@@ -0,0 +1,48 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1667210551405,
+  "repoUrl": "https://github.com/jonas-lj/bench_test",
+  "entries": {
+    "My Project Cargo Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Jonas Lindstrøm",
+            "username": "jonas-lj",
+            "email": "jonas-lj@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "7ea8979a4418f248fd1fc12afab789d1c5f3fc0c",
+          "message": "Update main.yml",
+          "timestamp": "2022-10-31T09:50:09Z",
+          "url": "https://github.com/jonas-lj/bench_test/commit/7ea8979a4418f248fd1fc12afab789d1c5f3fc0c"
+        },
+        "date": 1667210549563,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib/10",
+            "value": 244,
+            "range": "± 8",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/20",
+            "value": 32105,
+            "range": "± 1306",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/30",
+            "value": 3942918,
+            "range": "± 165801",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
